add61f98d52a23c63ce50093f1f033323c945f27
stupid cat <cat@blargbot.xyz>
2019-10-10 11:13:39 -0600

    styling fixes

--- components/Log.vue
+++ components/Log.vue
@@ -1,17 +1,19 @@
 <template>
   <div :class="'log ' + log.type">
-    <span
-      v-for="(segment, i) in log.processed.raw"
-      :key="i"
-      :class="segment.class"
-      :style="segment.style"
-    >
-      <template v-for="(part, j) in segment.parts">
-        <span :key="i + '_' + j" v-if="typeof part === 'string'">{{part}}</span>
-        <js-object :key="i + '_' + j" v-else-if="typeof part === 'object'" :obj="part" />
-      </template>
-    </span>
-    <div v-html="log.formatted"></div>
+    <div>
+      <span
+        v-for="(segment, i) in log.processed.raw"
+        :key="i"
+        :class="segment.class"
+        :style="segment.style"
+      >
+        <template v-for="(part, j) in segment.parts">
+          <span :key="i + '_' + j" v-if="typeof part === 'string'">{{part}}</span>
+          <js-object :key="i + '_' + j" v-else-if="typeof part === 'object'" :obj="part" />
+        </template>
+      </span>
+      <!-- <div v-html="log.formatted"></div> -->
+    </div>
   </div>
 </template>
 
