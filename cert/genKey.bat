d6b91d9603f3894d212b7950b7cd1244d6003685
Kirkland Morrill <cat@blargbot.xyz>
2020-03-06 12:27:03 -0700

    set days to 825

--- cert/genKey.bat
+++ cert/genKey.bat
@@ -4,4 +4,4 @@ rm interceptLocal.crt
 
 openssl genrsa -out interceptLocal.key 2048
 openssl req -new -key interceptLocal.key -subj "/C=US/ST=CA/O=intercept/CN=intercept" -out interceptLocal.csr
-openssl x509 -req -in interceptLocal.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out interceptLocal.crt -days 1825 -sha256 -extfile configuration.ext
+openssl x509 -req -in interceptLocal.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out interceptLocal.crt -days 825 -sha256 -extfile configuration.ext
